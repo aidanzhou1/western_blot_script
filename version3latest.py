199fc69487e6146a822ec4b4093fcf6e2590f415
Aidan Zhou <zhouaidan2004@gmail.com>
2021-03-06 13:45:00 -0800

    Updated version3 intro regression and classification

--- version3latest.py
+++ version3latest.py
@@ -57,13 +57,13 @@ def main(y, x):
         org = (50, 50) 
         
         # fontScale 
-        fontScale = 0.2
+        fontScale = 1
         
         # Blue color in BGR 
         color = (0, 0, 255) 
         
         # Line thickness of 2 px 
-        thickness = 1
+        thickness = 2
         
         # Using cv2.putText() method 
         # print(drawcontours[k][0].shape)
@@ -76,7 +76,7 @@ def main(y, x):
     original_imagergb = cv2.circle(original_imagergb, (int(posx), int(posy)), radius=1, color=(0, 0, 255), thickness=-1)
 
     #resize the entire image
-    original_imagergb = cv2.resize(original_imagergb, (500, 300))
+    original_imagergb = cv2.resize(original_imagergb, (1920, 900))
 
     # original_imagergb = cv2.resize(original_imagergb, (1920, 900))
     print("fin")
@@ -86,61 +86,17 @@ def main(y, x):
     thresholdglobal = cv2.getTrackbarPos('Threshold', 'img')
 
     cv2.destroyAllWindows()
-    
-
-
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    # # cv2.namedWindow('img')
-    # # cv2.setMouseCallback('img',draw_circle, cv_image)
-        
-            
 
         
-def draw_circle(event,x,y,flags, cv_image):
-    if event == cv2.EVENT_LBUTTONDBLCLK:
-        print("XY:" + str (x) + "_ " + str(y))
-        print("AAAAASSSSSSSSSSSSS" + str(cv_image[y][x]))
-        main(y,x)
+        
         
 def skimagefilter(y, x):
     global posx, posy
-    # skimage_image = io.imread("example_image/03102020_2.jpeg")
-    skimage_image = io.imread("example_image/01242020snippet.JPG")
+    # skimage_image = io.imread("example_image/testimage2.png")
+    skimage_image = io.imread("example_image/05142020_1.jpeg")
     skimage_image = rgb2gray(skimage_image)
-
-
-
-
-    # skimage_image = rotate(skimage_image, 90, resize=True)
-
-
-
-
-
     original_image = skimage_image
-
-    #reads the image
-    # skimage_image = rgb2gray(io.imread("example_image/05142020_1.jpeg"))
-    # skimage_image = rgb2gray(io.imread("example_image/9blackpixeltest.png"))
-
-
-    # image[a,b] a is the #rows (yaxis) b is the #columns (xaxis)
-    # this loops through all the rows from top to bottom
-
-    global ogheight, ogwidth
-    # # threshold is the background color number
-    # # print('!!!!!!!!!!!!')
-    # # print(y)
-    # # print(x)
-    # # print(skimage_image[int((y /960) * ogheight), int((x / 540) * ogwidth)])
-
     threshold1 = (skimage_image[0,0])
-    # threshold1 = (skimage_image[int(0.2 * len(skimage_image)), int(0.25 * len(skimage_image[0]))])
-    height, width = skimage_image.shape
-    print("Image Height:" + str(height))
-    # print('!!!' + str(int(0.25 * len(skimage_image[0]))))
-    # print('!!!' + str(int(0.2 * len(skimage_image))))
 
     posx = (int(0.25 * len(skimage_image[0])) / height ) * 540
     posy = (int(0.2 * len(skimage_image)) / width) * 960
@@ -154,25 +110,16 @@ def skimagefilter(y, x):
                 skimage_image[i, j] = skimage_image[i, j] + (1 - threshold1);
                 if (skimage_image[i, j] > 1):
                     skimage_image[i,j] = 1
-    # FOR GRAYSCALE 1 IS PURE WHITE, 0 IS PURE BLACK
     return original_image, skimage_image
 
 def skimagetocv(skimage_image):
-
     cv_image = img_as_ubyte(skimage_image)
     width, height = cv_image.shape[:2]
-
-
     return cv_image, width, height
 
 
 def threshold(cv_image, threshold):
-    thresh = cv2.adaptiveThreshold(cv_image,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,101,10)
-    # ret, thresh = cv2.threshold(cv_image, threshold, 255, cv2.THRESH_BINARY)
-    # cv2.imshow('imasdfsg', thresh)
-
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    thresh = cv2.adaptiveThreshold(cv_image,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,91,10)
     return thresh
 
 def cannyedge(cv_image):
@@ -199,87 +146,35 @@ def countcontours(cv_image, contours):
     print("Number of Contours: " + str(len(contours)))
     #for every contour
     for i in range(len(contours) - 1):
+
         mask = np.full_like(cv_image, 255)  # Create mask where white is what we want, black otherwise
-        
         #Its first argument is source image, second argument is the contours which should be passed as a Python list, third argument is index of contours 
         # (useful when drawing individual contour. To draw all contours, pass -1) and remaining arguments are color, thickness etc.
-        print("Primary size of mask: " + str(mask.shape))
+        print("Countour #:" + str(i))
         #skip the first contour
         mask = cv2.fillPoly(mask, pts =[contours[i + 1]], color=(0,0,0))
-        print("size of mask after fillpoly: " + str(mask.shape))
-
         out = np.full_like(cv_image, 255)  # Extract out the object and place into output image
-        print("size of out: " + str(out.shape))
 
-        
         #where mask = 0 make out the real darkness
         out[mask == 0] = cv_image[mask == 0]
         print("size of out after masking: " + str(out.shape))
         print("size of mask after masking: " + str(mask.shape))
-        
-
-        #TODO
-        #MAKE SURE THIS FUNCTION IS ACCURATE
-        #RIGHT NOW BLACKINMASK HAS MORE BLACK POINTS THAN BLACK IN OUT
-        #OUT IS SMALLER FOR SOME REASON
-        #TODO
-
 
         # Now crop
         (y, x) = np.where(mask == 0)
         (topy, topx) = (np.min(y), np.min(x))
         (bottomy, bottomx) = (np.max(y), np.max(x))
-
-
-        #NOTES
-        #no +1 on any: 12 10 17
-        #+1 on both: 10 9 14 
-        #+1 just on out: 10-9-14
-        #OUT IS A SQUARE WITH THE SHAPE INSIDE.
-
         mask = mask[topy:bottomy, topx : bottomx]
         out = out[topy + 1:bottomy - 1, topx + 1:bottomx - 1]
 
-        try:
-            mask = cv2.resize(mask, (500, 300))
-            out = cv2.resize(out, (500,300))
-        except Exception as e:
-            pass
-            
-        
-        # if ( i == 2):
-            
-        #     blackinmask = 0
-        #     blackinout = 0
-        #     for v in range (len(mask)):
-        #         for j in range(len(mask[i])):
-        #             if (mask[v,j] == 0):
-        #                 blackinmask = blackinmask + 1
-        #     for v in range (len(out)):
-        #         for j in range(len(out[i])):
-        #             if (out[v,j] == 0):
-        #                 blackinout = blackinout + 1
-        
-        #     print("blackinmask" + str(blackinmask))
-        #     print("blackinout" + str(blackinout))
         sumblack = 0
         times = 0
-        # mask = cv2.resize(mask, (1920, 900))
-        # cv2.imshow("mask", mask)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-        # out = cv2.resize(out, (1920, 900))
-        # cv2.imshow('asdfasdfasdf', out)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-
-        
         for y in range(len(out)):
             for x in range(len(out[y])):
-                # print("Mask[y,x]: " + str(mask[y,x]))
-                if (mask[y,x] == 0 and not out[y,x] == 255):
-                    sumblack = sumblack + out[y, x]
-                    times = times + 1
+                if (mask.size > 0 and mask[y].size > 0):
+                    if (mask[y,x] == 0 and not out[y,x] == 255):
+                        sumblack = sumblack + out[y, x]
+                        times = times + 1
                 # else:
                     # print("This Pixel is not black so the sum was not added")
         
